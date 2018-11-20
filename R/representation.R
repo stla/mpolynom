bba3827013f04b6346be2cdd1347463754389a59
stla <laurent_step@yahoo.fr>
2018-11-20 11:16:23 +0100

    roxygen doc

--- R/representation.R
+++ R/representation.R
@@ -25,17 +25,21 @@ degreesTable <- function(kmax){
 #' @rdname representation
 #' @name representation
 #' @param P a multipol
-#' @param allDegrees XXX the table returned by \code{\link{degreesTable}}, or 
-#' \code{NULL} for an automatic table
+#' @param degrees integer vector, the codings of the degrees of the monomials 
+#' of \code{P} with non-zero coefficients; this is for internal purpose, set to 
+#' \code{NULL} for an automatic derivation of this vector
 #' @param repr a representation of a polynomial: a list with \code{degrees} 
 #' and \code{coefficients}
 #' @param monomials a matrix giving the degrees of the monomials forming the 
-#' polynomial and their coding
+#' polynomial corresponding to \code{repr$degrees}
 #'
 #' @return The \code{representation} function returns a list with two elements: 
 #' the coefficients in a numeric vector (\code{coefficients}) and an integer 
 #' vector representing the monomial terms (\code{degrees}).
 #' The \code{backrepresentation} function returns a \link[multipol]{multipol}.
+#' 
+#' @note These functions, especially \code{backrepresentation}, are rather for 
+#' internal purposes.
 #' 
 #' @importFrom prodlim row.match
 #'
